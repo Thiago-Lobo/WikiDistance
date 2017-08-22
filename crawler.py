cfb7cd137c64b7328717305f51e2f93e871fa905
Thiago Costa Lobo <thiagolobo@poli.ufrj.br>
2017-08-22 00:29:09 -0300

    Adds local cache to crawler

--- crawler.py
+++ crawler.py
@@ -5,6 +5,8 @@ from py2neo import Graph, Relationship, authenticate, Node
 from datetime import datetime as dt
 import random
 
+local_cache = []
+
 base_url = 'https://pt.wikipedia.org/w/api.php'
 query_url = base_url + '?action=query&format=json'
 backlinks_url = query_url + '&list=backlinks&blredirect=1&blnamespace=0&bllimit=max'
@@ -12,6 +14,7 @@ properties_url = query_url + '&prop=info|links'
 children_url = query_url + '&prop=info|links&inprop=url&plnamespace=0&pllimit=max'
 
 db_host = 'localhost'
+# db_host = '192.168.1.4'
 db_port = '7474'
 
 graph = None
@@ -21,6 +24,7 @@ transaction = None
 crawler_start_url = 'https://pt.wikipedia.org/wiki/Kobi_Lichtenstein'
 
 api_request_counter = 0
+visits_counter = 0
 
 def initialize_neo4j():
 	global graph
@@ -30,6 +34,15 @@ def initialize_neo4j():
 
 	graph.run("CREATE CONSTRAINT ON (a:Article) ASSERT a.page_id IS UNIQUE")
 	graph.run("CREATE CONSTRAINT ON (a:Article) ASSERT a.title IS UNIQUE")
+
+def update_cache():
+	global local_cache
+
+	result = graph.data("MATCH (n:Article) WHERE n.visited=false RETURN n")
+
+	local_cache = [x["n"]["title"] for x in result]
+
+	print u"[{}] Local cache updated. Cache size: {}".format(current_time_string(), len(local_cache)).encode('utf-8')
 
 def current_time_string():
 	return dt.now().strftime("%Y-%m-%d %H:%M:%S.%f")
@@ -93,18 +106,29 @@ def push_article(metadata, visited, invalid = False):
 		node["visited"] = visited
 		node["invalid"] = invalid
 
-		node.push		
+		node.push()	
 
 	return node
 
 def relate_articles(parent, child):
 	graph.merge(Relationship(parent, "CONTAINS", child))
 
+def delete_article(title):
+	graph.run(u"MATCH (a:Article) WHERE a.title='{}' DETACH DELETE a".format(title).encode('utf-8'))
+
 def visit_article(title):
+	global visits_counter
+
 	data = query_article_data_by_title(title)
 
+	visits_counter += 1
+
+	if title in local_cache:
+		# print u"[{}] Removing '{}' from local cache. Cache size: {}".format(current_time_string(), title, len(local_cache)).encode('utf-8')
+		local_cache.remove(title)
+
 	if data == -1:
-		push_article({'title': title}, True, True)
+		delete_article(title)
 		return
 
 	parent_article = push_article(data["metadata"], True)
@@ -114,11 +138,17 @@ def visit_article(title):
 	for counter, article in enumerate(data["links"]):
 		print u"[{}] Pushing child article {}/{} with title: '{}'".format(current_time_string(), counter, len(data["links"]), article["title"]).encode('utf-8')
 
+		if article["title"] in local_cache:
+			# print u"[{}] Skipping cached article".format(current_time_string()).encode('utf-8')
+			continue
+
 		if article["title"] == data["metadata"]["title"]:
 			print u"[{}] Skipping loop".format(current_time_string()).encode('utf-8')
 			continue
 
 		metadata = {'title': article["title"]}
+		# print u"[{}] Inserting '{}' into local cache. Cache size: {}".format(current_time_string(), article["title"], len(local_cache)).encode('utf-8')
+		local_cache.append(article["title"])
 		child_article = push_article(metadata, False)
 		relate_articles(parent_article, child_article)
 		
@@ -133,12 +163,16 @@ def get_unvisited_article():
 	if len(result) == 0:
 		return "_____1noarticle"
 
-	return result[random.randint(0, len(result) - 1)]["a"]["title"]
+	# return result[random.randint(0, len(result) - 1)]["a"]["title"]
+	return result[0]["a"]["title"]
 
 def start():
+	global visits_counter
+
 	random.seed(dt.now())
 	initialize_neo4j()
-	
+	update_cache()
+
 	if query_article_count() == 0:
 		visit_article(parse_title_from_url(crawler_start_url))
 
@@ -146,6 +180,8 @@ def start():
 
 	while unvisited_title != "_____1noarticle":
 		try:
+			if visits_counter != 0 and visits_counter % 1000 == 0:
+				update_cache()
 			visit_article(unvisited_title)
 			unvisited_title = get_unvisited_article()
 		except Exception, e:
