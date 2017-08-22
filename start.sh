c57032df1f132c5895d34b5e85a1d2a874469204
Thiago Lobo <thiagolobo@poli.ufrj.br>
2017-08-21 23:27:02 -0300

    Adds startup script

---
+++ start.sh
@@ -0,0 +1,7 @@
+nohup python crawler.py > output1.txt 2>&1 &
+sleep 30
+nohup python crawler.py > output2.txt 2>&1 &
+sleep 30
+nohup python crawler.py > output3.txt 2>&1 &
+sleep 30
+nohup python crawler.py > output4.txt 2>&1 &
